02a0fe5127abbb9a62f552c240d60f4f6cb3904a
Varun Munjeti <vrunjeti@gmail.com>
2016-03-25 03:14:22 -0500

    visualized comment data

--- src/scripts/components/Chart.js
+++ src/scripts/components/Chart.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { ScatterChart } from 'react-d3'
+import request from 'superagent'
 
 export default class Chart extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = { data: [{ values: [] }] }
+  }
+
+  componentDidMount() {
+    request.get('./../../../data/comments_processed.json')
+      .then(res => {
+        this.setState({
+          data: [{
+            name: 'comments',
+            values: res.body.map(comment => ({ x: comment.sentiment, y: comment.likes }))
+          }]
+        })
+      })
   }
 
   render() {
+    const { data } = this.state
     return (
-      <div>Hi</div>
+      <div className="chart-container">
+        { data[0].values.length && <ScatterChart
+          data={data}
+          width={800}
+          height={800}
+          title="Sentiment Polarity vs # Likes for comments on my Facebook statuses"
+          xAxisLabel="Sentiment Polarity"
+          yAxisLabel="# Likes"
+        /> }
+      </div>
     )
   }
 }
