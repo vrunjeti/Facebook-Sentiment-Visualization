4d1660999a5a4a97f8ead4c990b04f9d7412619a
Varun Munjeti <vrunjeti@gmail.com>
2016-03-25 03:48:05 -0500

    visualized status data

--- src/scripts/components/Chart.js
+++ src/scripts/components/Chart.js
@@ -6,30 +6,50 @@ import request from 'superagent'
 export default class Chart extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { data: [{ values: [] }] }
+    this.state = {
+      commentData: [{ values: [] }],
+      statusData: [{ values: [] }]
+    }
   }
 
   componentDidMount() {
-    request.get('./../../../data/comments_processed.json')
-      .then(res => {
+    Promise.all([
+      request.get('./../../../data/comments_processed.json'),
+      request.get('./../../../data/statuses_processed.json')
+    ]).then(res => {
+        const [ comments, statuses ] = res
         this.setState({
-          data: [{
+          commentData: [{
             name: 'comments',
-            values: res.body.map(comment => ({ x: comment.sentiment, y: comment.likes }))
+            values: comments.body.map(comment => ({ x: comment.sentiment, y: comment.likes }))
+          }],
+          statusData: [{
+            name: 'statuses',
+            values: statuses.body.map(status => ({ x: status.sentiment, y: status.likes }))
           }]
         })
       })
   }
 
   render() {
-    const { data } = this.state
+    const { commentData, statusData } = this.state
     return (
       <div className="chart-container">
-        { data[0].values.length && <ScatterChart
-          data={data}
+        <div className="filler"></div>
+        { commentData[0].values.length && <ScatterChart
+          data={commentData}
           width={800}
           height={800}
           title="Sentiment Polarity vs # Likes for comments on my Facebook statuses"
+          xAxisLabel="Sentiment Polarity"
+          yAxisLabel="# Likes"
+        /> }
+        <div className="filler"></div>
+        { statusData[0].values.length && <ScatterChart
+          data={statusData}
+          width={800}
+          height={800}
+          title="Sentiment Polarity vs # Likes for my Facebook statuses"
           xAxisLabel="Sentiment Polarity"
           yAxisLabel="# Likes"
         /> }
